abb51e56d55c7790a7484141ee038ccd6d39ae1e
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-11-25 15:30:33 +0900

    chore: ESLint 설정

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import eslintPluginTs from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    ignores: ["dist/**", "node_modules/**"],
+  },
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": eslintPluginTs,
+    },
+    rules: {},
+  },
+];
